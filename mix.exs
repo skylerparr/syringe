3c495b1bf305ef112afd190ffee56011859b59e4
Skyler Parr <skylerparr@gmail.com>
2016-08-28 17:11:19 +0000

    changed mix exs to a lower version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Injector.Mixfile do
   def project do
     [app: :injector,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
