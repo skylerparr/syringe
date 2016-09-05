279c610ee4400381512288381606f6fea9269c2d
Skyler Parr <skyler.parr@breakoutbit.com>
2016-09-05 13:39:42 -0700

    added ex_doc

--- mix.exs
+++ mix.exs
@@ -27,7 +27,9 @@ defmodule Injector.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+    {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 
   defp description do
