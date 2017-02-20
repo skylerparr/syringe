b4755e58c8f5a6c49d292305e3491b3965917734
Skyler Parr <skyler@tachaeon.com>
2017-02-19 17:11:49 -0800

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Injector.Mixfile do
 
   def project do
     [app: :syringe,
-     version: "0.10.2",
+     version: "0.10.3",
      elixir: "~> 1.2",
      description: description(),
      package: package(),
