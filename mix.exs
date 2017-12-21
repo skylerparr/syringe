caea19b5c0d8501f02f739365aaa9411e4f2c1cc
Skyler Parr <skylerparr@gmail.com>
2017-12-21 12:18:17 -0800

    bumped version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Injector.Mixfile do
 
   def project do
     [app: :syringe,
-     version: "0.10.11",
+     version: "0.10.12",
      elixir: "~> 1.2",
      description: description(),
      package: package(),
