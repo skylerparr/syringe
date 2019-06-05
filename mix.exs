2ba8da1f7abca60cd14fc34f449dae6400ebb76f
Skyler Parr <skylerparr@gmail.com>
2019-06-04 21:50:00 -0400

    update ex_doc version

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Injector.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-    {:ex_doc, ">= 0.0.0", only: :dev}
+    {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 
